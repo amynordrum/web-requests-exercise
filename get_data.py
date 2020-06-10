5d2eff62d47e311b45f1a31c8d0d0f534d2f79c6
amynordrum <amynordrum@users.noreply.github.com>
2020-06-09 20:21:14 -0400

    Added requests package

--- get_data.py
+++ get_data.py
@@ -1,3 +1,9 @@
 # get_data.py
 
-print("REQUESTING SOME DATA FROM THE INTERNET...")
+import requests
+
+request_url = "https://raw.githubusercontent.com/prof-rossetti/intro-to-python/master/data/products/2.json"
+response = requests.get(request_url)
+print(response.status_code)
+print(response.text)
+
